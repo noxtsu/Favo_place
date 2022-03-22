69a330e3efb303fb909acabbdd07445589a91dc1
noxtsu <kuraraiban29@gmail.com>
2022-03-22 13:07:48 +0000

    [add]user edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_image])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users do
+  #devise_for :users, controllers: {
+  # registrations: 'users/registrations',
+  # sessions: 'users/sessions'
+  #}
+ #  devise_for :users, controllers: {registrations: 'users/registrations'}
+  devise_for :users, controllers: {registrations: 'users/registrations'} do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   member do
     get :follows, :followers
@@ -23,7 +28,7 @@ Rails.application.routes.draw do
 
   resources :posts do
   resources :comments, only:[:create, :destroy]
-  resources :favorites, only: [:create, :destroy]
+  resource :favorites, only: [:create, :destroy]
   end
   #get 'posts/index'
   #get 'posts/show'
